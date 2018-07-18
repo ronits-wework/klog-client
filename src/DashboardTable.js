16e8384024ceb48314401691c1920d3e24258b52
Tomer Desser <tomer.desser@wework.com>
2018-07-18 21:07:54 +0300

    Basic modal open and close

--- src/DashboardTable.js
+++ src/DashboardTable.js
@@ -1,8 +1,23 @@
 import ReactTable from 'react-table'
 import React, {Component} from 'react';
 import 'react-table/react-table.css'
+import Modal from 'react-modal';
+
+Modal.setAppElement('body')
 
 class DashboardTable extends Component {
+  state = {
+    isModalOpen: false
+  };
+
+  handleClick = (state, rowInfo) => {
+    const id = rowInfo.original.id
+    this.setState({isModalOpen: true})
+  }
+
+  closeModal = () => {
+    this.setState({isModalOpen: false});
+  }
 
   render() {
     const data = this.props.complains
@@ -18,10 +33,24 @@ class DashboardTable extends Component {
       },
     ]
 
-    return <ReactTable
-      data={data}
-      columns={columns}
-    />
+    return <div>
+      <ReactTable
+        getTdProps={(state, rowInfo) => {
+          return {onClick: () => this.handleClick(state, rowInfo)}
+        }}
+        data={data}
+        columns={columns}
+      />
+
+      <Modal
+        isOpen={this.state.isModalOpen}
+        onRequestClose={this.closeModal}
+        contentLabel="Respond"
+      >
+        <div>HIIII</div>
+        <button onClick={this.closeModal}>close</button>
+      </Modal>
+    </div>
   }
 
 }
