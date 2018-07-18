33d18b6c74c6d5d150a39921229e284594b56029
Tomer Desser <tomer.desser@wework.com>
2018-07-18 21:32:24 +0300

    working with response

--- src/DashboardTable.js
+++ src/DashboardTable.js
@@ -2,21 +2,48 @@ import ReactTable from 'react-table'
 import React, {Component} from 'react';
 import 'react-table/react-table.css'
 import Modal from 'react-modal';
+import {dataProvider} from "./images/DataProvider";
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 Modal.setAppElement('body')
 
 class DashboardTable extends Component {
   state = {
-    isModalOpen: false
+    isModalOpen: false,
+    response: {
+      id: '',
+      text: ''
+    }
   };
 
   handleClick = (state, rowInfo) => {
-    const id = rowInfo.original.id
+    var response = {...this.state.response}
+    response.id = rowInfo.original.id;
+    this.setState({response})
     this.setState({isModalOpen: true})
   }
 
-  closeModal = () => {
+  SendResponse = () => {
+    const response = this.state.response;
+
     this.setState({isModalOpen: false});
+    dataProvider.post('https://klog-staging.herokuapp.com/api/v1/complaint/reply', {id: response.id, message: response.text})
+  }
+
+  handleChangeResponse = (e) => {
+    var response = {...this.state.response}
+    response.text = e.target.value;
+    this.setState({response})
   }
 
   render() {
@@ -44,11 +71,17 @@ class DashboardTable extends Component {
 
       <Modal
         isOpen={this.state.isModalOpen}
-        onRequestClose={this.closeModal}
+        onRequestClose={this.SendResponse}
         contentLabel="Respond"
+        style={customStyles}
       >
-        <div>HIIII</div>
-        <button onClick={this.closeModal}>close</button>
+        <div>Respond to the poor guy</div>
+        <input
+          type="text"
+          value={this.state.response.text}
+          onChange={this.handleChangeResponse}
+        />
+        <button onClick={this.SendResponse}>Send</button>
       </Modal>
     </div>
   }
