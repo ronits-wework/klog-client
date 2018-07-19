659c0d551e799d3ef9009c83b1e1f897817e0e35
Ronit Steinberg <ronit.steinberg@wework.com>
2018-07-19 10:28:51 +0300

    update stuff

---
+++ src/Analytics.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { dataProvider } from "./images/DataProvider";
+import {Line as LineChart} from 'react-chartjs';
+
+class Analytics extends Component {
+  state = {
+    complaints: [],
+    perDayData: this.getPerDayComplaints()
+  };
+
+  setComplaints = (complaints) => {
+    const perDayData = this.getPerDayComplaints(complaints);
+    this.setState({ complaints, perDayData: perDayData });
+  };
+
+  componentWillMount (){
+    dataProvider.get('https://klog-staging.herokuapp.com/api/v1/complaint')
+      .then(this.setComplaints)
+      .catch(console.error);
+  }
+
+  formatDate(date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+
+    return day + ' ' + monthNames[monthIndex];
+  }
+
+  getPerDayComplaints(complaints) {
+
+    let datesHash = {};
+    let dates = [];
+    let data = [];
+
+    if (complaints != null) {
+      for (let i = 0; i < complaints.length; i++) {
+        const created_at = new Date(complaints[i].created_at);
+        const key = created_at.getDate() + '.' + created_at.getMonth();
+        if (datesHash[key] == null) {
+          datesHash[key] = 1;
+        }
+        else {
+          datesHash[key]++;
+        }
+      }
+    }
+
+    for (let i = 0; i < 7; i++) {
+      let date = new Date();
+      date.setDate(date.getDate() - i);
+      const currIndex = 7-i-1;
+      dates[currIndex] = this.formatDate(date);
+      const key = date.getDate()+'.'+date.getMonth();
+
+      if (datesHash[key] == null) {
+        data[currIndex] = 0;
+      }
+      else {
+        data[currIndex] = datesHash[key];
+      }
+    }
+
+    const chartData = {
+      labels: dates,
+      datasets: [
+        {
+          label: 'Complaints per day',
+          fillColor: 'rgba(151,187,205,0.2)',
+          strokeColor: 'rgba(151,187,205,1)',
+          pointColor: 'rgba(151,187,205,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(151,187,205,1)',
+          data: data,
+        }
+      ]
+    };
+
+    const chartOptions = {
+      scaleShowGridLines: true,
+      scaleGridLineColor: 'rgba(0,0,0,.05)',
+      scaleGridLineWidth: 1,
+      scaleShowHorizontalLines: true,
+      scaleShowVerticalLines: true,
+      bezierCurve: true,
+      bezierCurveTension: 0.4,
+      pointDot: true,
+      pointDotRadius: 4,
+      pointDotStrokeWidth: 1,
+      pointHitDetectionRadius: 20,
+      datasetStroke: true,
+      datasetStrokeWidth: 2,
+      datasetFill: true,
+      legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+    };
+
+    const styles = {
+      graphContainer: {
+        border: '1px solid black',
+        padding: '15px'
+      }
+    };
+
+    return {
+      chartData: chartData,
+      chartOptions: chartOptions,
+      styles: styles
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <LineChart data={this.state.perDayData.chartData} options={this.state.perDayData.chartOptions} styles={this.state.perDayData.styles} width="600" height="250"/>
+      </div>
+    );
+  }
+}
+
+export default Analytics;
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from "./Main";
 import Dashboard from "./Dashboard";
+import Analytics from "./Analytics";
 
 export default function Layout () {
   return (
@@ -10,6 +11,7 @@ export default function Layout () {
         <div className="app-container">
           <Route exact path='/' component={Main}/>
           <Route exact path='/dashboard' component={Dashboard}/>
+          <Route exact path='/analytics' component={Analytics}/>
         </div>
       </BrowserRouter>
     </div>
